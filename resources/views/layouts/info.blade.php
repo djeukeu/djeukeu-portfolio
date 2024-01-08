0867de0376606543100d305cadf723f7ceadc148
djeukeu <dmxchristian@gmail.com>
2024-01-08 13:50:39 +0000

    info

--- resources/views/layouts/info.blade.php
+++ resources/views/layouts/info.blade.php
@@ -1,6 +1,5 @@
 <div class="art-info-bar">
 
-    <!-- menu bar frame -->
     <div class="art-info-bar-frame">
 
         <!-- info bar header -->
@@ -30,200 +29,90 @@
             </div>
             <!-- avatar end -->
             <!-- name -->
-            <h5 class="art-name mb-10">Artur Carter</h5>
+            <h5 class="art-name mb-10">{{ $name }}</h5>
             <!-- post -->
-            <div class="art-sm-text">Front-end Deweloper <br>Ui/UX Designer, </div>
+            <div class="art-sm-text">Software Developer, <br>Cloud Developer</div>
         </div>
         <!-- info bar header end -->
 
         <!-- scroll frame -->
         <div id="scrollbar2" class="art-scroll-frame">
 
-            <!-- info bar about -->
             <div class="art-table p-15-15">
-                <!-- about text -->
                 <ul>
-                    <!-- country -->
                     <li>
-                        <h6>Residence:</h6><span>Canada</span>
+                        <h6>Residence:</h6><span>United Kingdom</span>
                     </li>
-                    <!-- city -->
                     <li>
-                        <h6>City:</h6><span>Toronto</span>
+                        <h6>City:</h6><span>Leeds</span>
                     </li>
-                    <!-- age -->
                     <li>
-                        <h6>Age:</h6><span>26</span>
+                        <h6>Age:</h6><span><?php $dob = '2000-01-12';
+                        $year = date('Y') - date('Y', strtotime($dob));
+                        echo $year;
+                        ?></span>
                     </li>
                 </ul>
             </div>
-            <!-- info bar about end -->
 
             <!-- divider -->
             <div class="art-ls-divider"></div>
 
-            <!-- language skills -->
+            <!-- language skills-->
             <div class="art-lang-skills p-30-15">
 
                 <!-- skill -->
                 <div class="art-lang-skills-item">
                     <div id="circleprog1" class="art-cirkle-progress"></div>
-                    <!-- title -->
-                    <h6>French</h6>
+                    <h6>English</h6>
                 </div>
-                <!-- skill end -->
+                <!-- skill -->
 
                 <!-- skill -->
                 <div class="art-lang-skills-item">
                     <div id="circleprog2" class="art-cirkle-progress"></div>
-                    <!-- title -->
-                    <h6>English</h6>
+                    <h6>French</h6>
                 </div>
-                <!-- skill end -->
-
-                <!-- skill -->
-                <div class="art-lang-skills-item">
-                    <div id="circleprog3" class="art-cirkle-progress"></div>
-                    <!-- title -->
-                    <h6>Spanish</h6>
-                </div>
-                <!-- skill end -->
+                <!-- skill  -->
 
             </div>
-            <!-- language skills end -->
+            <!-- language skills -->
 
             <!-- divider -->
             <div class="art-ls-divider"></div>
 
-            <!-- hard skills -->
-            <div class="art-hard-skills p-30-15">
-
-                <!-- skill -->
-                <div class="art-hard-skills-item">
-                    <div class="art-skill-heading">
-                        <!-- title -->
-                        <h6>html</h6>
-                    </div>
-                    <!-- progressbar frame -->
-                    <div class="art-line-progress">
-                        <!-- progressbar -->
-                        <div id="lineprog1"></div>
-                    </div>
-                    <!-- progressbar frame end -->
-                </div>
-                <!-- skill end -->
-
-                <!-- skill -->
-                <div class="art-hard-skills-item">
-                    <div class="art-skill-heading">
-                        <!-- title -->
-                        <h6>CSS</h6>
-                    </div>
-                    <!-- progressbar frame -->
-                    <div class="art-line-progress">
-                        <!-- progressbar -->
-                        <div id="lineprog2"></div>
-                    </div>
-                    <!-- progressbar frame end -->
-                </div>
-                <!-- skill end -->
-
-                <!-- skill -->
-                <div class="art-hard-skills-item">
-                    <div class="art-skill-heading">
-                        <!-- title -->
-                        <h6>Js</h6>
-                    </div>
-                    <!-- progressbar frame -->
-                    <div class="art-line-progress">
-                        <!-- progressbar -->
-                        <div id="lineprog3"></div>
-                    </div>
-                    <!-- progressbar frame end -->
-                </div>
-                <!-- skill end -->
-
-                <!-- skill -->
-                <div class="art-hard-skills-item">
-                    <div class="art-skill-heading">
-                        <!-- title -->
-                        <h6>PHP</h6>
-                    </div>
-                    <!-- progressbar frame -->
-                    <div class="art-line-progress">
-                        <!-- progressbar -->
-                        <div id="lineprog4"></div>
-                    </div>
-                    <!-- progressbar frame end -->
-                </div>
-                <!-- skill end -->
-
-                <!-- skill -->
-                <div class="art-hard-skills-item">
-                    <div class="art-skill-heading">
-                        <!-- title -->
-                        <h6>Wordpress</h6>
-                    </div>
-                    <!-- progressbar frame -->
-                    <div class="art-line-progress">
-                        <!-- progressbar -->
-                        <div id="lineprog5"></div>
-                    </div>
-                    <!-- progressbar frame end -->
-                </div>
-                <!-- skill end -->
-
-            </div>
-            <!-- language skills end -->
-
-            <!-- divider -->
-            <div class="art-ls-divider"></div>
-
-            <!-- knowledge list -->
+            <!-- hobbies list -->
             <ul class="art-knowledge-list p-15-0">
-                <!-- list item -->
-                <li>Bootstrap, Materialize</li>
-                <!-- list item -->
-                <li>Stylus, Sass, Less</li>
-                <!-- list item -->
-                <li>Gulp, Webpack, Grunt</li>
-                <!-- list item -->
-                <li>GIT knowledge</li>
+                <li>Video games</li>
+                <li>Chess</li>
+                <li>Reading</li>
+                <li>Learning</li>
             </ul>
-            <!-- knowledge list end -->
+            <!-- hobbies list -->
 
             <!-- divider -->
             <div class="art-ls-divider"></div>
 
-            <!-- links frame -->
+            <!-- download cv -->
             <div class="art-links-frame p-15-15">
-
-                <!-- download cv button -->
-                <a href="files/cv.txt" class="art-link" download data-no-swup>Download cv <i
+                <a href="files/djeukeu_cv.pdf" class="art-link" download data-no-swup>Download cv <i
                         class="fas fa-download"></i></a>
-
             </div>
-            <!-- links frame end -->
+            <!-- download cv -->
 
         </div>
         <!-- scroll frame end -->
 
         <!-- sidebar social -->
         <div class="art-ls-social">
-            <!-- social link -->
-            <a href="#." target="_blank"><i class="fab fa-linkedin-in"></i></a>
-            <!-- social link -->
-            <a href="#." target="_blank"><i class="fab fa-dribbble"></i></a>
-            <!-- social link -->
-            <a href="#." target="_blank"><i class="fab fa-behance"></i></a>
-            <!-- social link -->
-            <a href="#." target="_blank"><i class="fab fa-github"></i></a>
-            <!-- social link -->
-            <a href="#." target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/djeukeu/" target="_blank" data-no-swup><i
+                    class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/djeukeu" target="_blank" data-no-swup><i class="fab fa-github"></i></a>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
         <!-- sidebar social end -->
 
     </div>
-    <!-- menu bar frame end -->
-
 </div>
