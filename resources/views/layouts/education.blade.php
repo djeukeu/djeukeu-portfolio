cb0d366a1ae365968477798d3df1dd41e28a41d1
djeukeu <dmxchristian@gmail.com>
2024-01-08 19:28:40 +0000

    education

--- resources/views/layouts/education.blade.php
+++ resources/views/layouts/education.blade.php
@@ -1,117 +1,156 @@
 <div class="container-fluid">
 
-    <!-- row -->
     <div class="row">
 
-        <!-- col -->
+        <!-- Education -->
         <div class="col-lg-6">
-
-            <!-- section title -->
             <div class="art-section-title">
-                <!-- title frame -->
                 <div class="art-title-frame">
-                    <!-- title -->
                     <h4>Education</h4>
                 </div>
-                <!-- title frame end -->
             </div>
-            <!-- section title end -->
 
-            <!-- timeline -->
             <div class="art-timeline art-gallery" id="history">
+
+                <!-- Single Education -->
                 <div class="art-timeline-item">
                     <div class="art-timeline-mark-light"></div>
                     <div class="art-timeline-mark"></div>
-
                     <div class="art-a art-timeline-content">
                         <div class="art-card-header">
                             <div class="art-left-side">
-                                <h5>Title of section 1</h5>
-                                <div class="art-el-suptitle mb-15">Template author</div>
+                                <h5>Bachelor of Technology</h5>
+                                <div class="art-el-suptitle mb-15">Computer Engineering</div>
                             </div>
                             <div class="art-right-side">
-                                <span class="art-date">jan 2018 - may 2020</span>
+                                <span class="art-date">Oct 2019 - Jun 2020</span>
                             </div>
                         </div>
-
-                        <p>Dolor sit amet, consectetur adipisicing elit. Iusto, optio,
-                            dolorum provident rerum aut hic quasi placeat iure tempora
-                            laudantium ipsa ad debitis unde? Iste voluptatibus minus
-                            veritatis qui ut.</p>
-                        <a data-fancybox="diplome" data-no-swup href="files/certificate.jpg"
-                            class="art-link art-color-link art-w-chevron">Diplome</a>
+                        <div>Graduated from <a href="https://myiuc.com/en/home-en/" target="_blank"
+                                class="art-link art-color-link" data-no-swup>INSTITUT UNIVERSITAIRE DE
+                                LA CÔTE</a> with a Bachelor's degree in Software Engineering.</div>
                     </div>
                 </div>
+                <!-- Single Education -->
 
+                <!-- Single Education -->
                 <div class="art-timeline-item">
                     <div class="art-timeline-mark-light"></div>
                     <div class="art-timeline-mark"></div>
-
                     <div class="art-a art-timeline-content">
                         <div class="art-card-header">
                             <div class="art-left-side">
-                                <h5>Title of section 1</h5>
-                                <div class="art-el-suptitle mb-15">Template author</div>
+                                <h5>Higher National Diploma</h5>
+                                <div class="art-el-suptitle mb-15">Computer Engineering</div>
                             </div>
                             <div class="art-right-side">
-                                <span class="art-date">jan 2018 - may 2020</span>
+                                <span class="art-date">Oct 2017 - Jul 2019</span>
                             </div>
                         </div>
-                        <div>Consectetur adipisicing elit. Iusto, optio, dolorum provident
-                            rerum aut hic quasi placeat iure tempora laudantium ipsa ad
-                            debitis unde?</div>
+                        <p>HND in Software Engineering and Computing from<a href="https://myiuc.com/en/home-en/"
+                                target="_blank" class="art-link art-color-link" data-no-swup>INSTITUT UNIVERSITAIRE DE
+                                LA CÔTE</a></p>
                     </div>
                 </div>
+                <!-- Single Education -->
 
+                <!-- Single Education -->
                 <div class="art-timeline-item">
                     <div class="art-timeline-mark-light"></div>
                     <div class="art-timeline-mark"></div>
-
                     <div class="art-a art-timeline-content">
                         <div class="art-card-header">
                             <div class="art-left-side">
-                                <h5>Title of section 1</h5>
-                                <div class="art-el-suptitle mb-15">Template author</div>
+                                <h5>AWS Certified Cloud Practitioner</h5>
+                                <div class="art-el-suptitle mb-15">Amazon Web Services (AWS)</div>
                             </div>
                             <div class="art-right-side">
-                                <span class="art-date">jan 2018 - may 2020</span>
+                                <span class="art-date">Issued Dec 2023</span>
                             </div>
                         </div>
-                        <p>Dolorum provident rerum aut hic quasi placeat iure tempora
-                            laudantium ipsa ad debitis unde? Iste voluptatibus minus
-                            veritatis qui ut.</p>
-                        <a data-fancybox="diplome" data-no-swup href="files/certificate.jpg"
-                            class="art-link art-color-link art-w-chevron">Licence</a>
+                        <p>This certificate demonstrates my understanding of IT services, cloud fluency and foundational
+                            knowledge of AWS.</p>
+                        <a data-no-swup target="_blank"
+                            href="https://www.credly.com/badges/84178e49-ac39-43fd-aca1-d70f46b41e46/linked_in_profile"
+                            class="art-link art-color-link art-w-chevron">Verify</a>
                     </div>
-
                 </div>
+                <!-- Single Education -->
 
+                <!-- Single Education -->
                 <div class="art-timeline-item">
                     <div class="art-timeline-mark-light"></div>
                     <div class="art-timeline-mark"></div>
-
                     <div class="art-a art-timeline-content">
                         <div class="art-card-header">
                             <div class="art-left-side">
-                                <h5>Title of section 1</h5>
-                                <div class="art-el-suptitle mb-15">Template author</div>
+                                <h5>NodeJS</h5>
+                                <div class="art-el-suptitle mb-15">Udemy</div>
                             </div>
                             <div class="art-right-side">
-                                <span class="art-date">jan 2018 - may 2020</span>
+                                <span class="art-date">Issued Dec 2023</span>
                             </div>
                         </div>
-                        <p>Ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio,
-                            dolorum provident rerum.</p>
-                        <a data-fancybox="diplome" data-no-swup href="files/certificate.jpg"
-                            class="art-link art-color-link art-w-chevron">Certificate</a>
+                        <p>This certificate indicates that I have completed the entire NodeJS - The Complete Guide (MVC,
+                            REST APIs, GraphQL, Deno) course.</p>
+                        <a data-no-swup target="_blank"
+                            href="https://www.udemy.com/certificate/UC-0d4f345f-bf71-45be-9acb-32cfe154fa66/"
+                            class="art-link art-color-link art-w-chevron">Verify</a>
                     </div>
-
                 </div>
+                <!-- Single Education -->
+
+                <!-- Single Education -->
+                <div class="art-timeline-item">
+                    <div class="art-timeline-mark-light"></div>
+                    <div class="art-timeline-mark"></div>
+                    <div class="art-a art-timeline-content">
+                        <div class="art-card-header">
+                            <div class="art-left-side">
+                                <h5>React</h5>
+                                <div class="art-el-suptitle mb-15">Udemy</div>
+                            </div>
+                            <div class="art-right-side">
+                                <span class="art-date">Issued Dec 2023</span>
+                            </div>
+                        </div>
+                        <p>This certificate indicates that I have completed the entire React - The Complete Guide 2023
+                            (incl. React Router & Redux) course.</p>
+                        <a data-no-swup target="_blank"
+                            href="https://www.udemy.com/certificate/UC-16afeee7-aca7-42b8-804a-22119526e047/"
+                            class="art-link art-color-link art-w-chevron">Verify</a>
+                    </div>
+                </div>
+                <!-- Single Education -->
+
+                <!-- Single Education -->
+                <div class="art-timeline-item">
+                    <div class="art-timeline-mark-light"></div>
+                    <div class="art-timeline-mark"></div>
+                    <div class="art-a art-timeline-content">
+                        <div class="art-card-header">
+                            <div class="art-left-side">
+                                <h5>React Native</h5>
+                                <div class="art-el-suptitle mb-15">Udemy</div>
+                            </div>
+                            <div class="art-right-side">
+                                <span class="art-date">Issued Dec 2023</span>
+                            </div>
+                        </div>
+                        <p>This certificate indicates that I have completed the entire React Native - The Practical
+                            Guide [2023] course.</p>
+                        <a data-no-swup target="_blank"
+                            href="https://www.udemy.com/certificate/UC-fb0abddc-35fb-4573-aa2b-d06e072bac76/"
+                            class="art-link art-color-link art-w-chevron">Verify</a>
+                    </div>
+                </div>
+                <!-- Single Education -->
 
             </div>
-            <!-- timeline end -->
 
         </div>
+        <!-- Education -->
+
         <div class="col-lg-6">
 
             <!-- section title -->
