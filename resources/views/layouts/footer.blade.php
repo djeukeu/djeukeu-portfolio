f56ed350da25bbc8ac7dcc65dbb6f5f8d7bcdd49
djeukeu <dmxchristian@gmail.com>
2024-01-08 11:49:31 +0000

    structure

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+
+    <!-- footer -->
+    <footer>
+        <!-- copyright -->
+        <div class="art-copy">© 2021 Artur Carter</div>
+        <!-- author ( Please! Do not delete it. You are awesome! :) -->
+        <div>Template author:&#160; <a
+                href="https://themeforest.net/user/millerdigitaldesign" target="_blank"
+                data-no-swup>Nazar Miller</a></div>
+    </footer>
+    <!-- footer end -->
+
+</div>
