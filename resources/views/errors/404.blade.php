285a630edd24aab2754f2540787e4a8e070e359c
djeukeu <dmxchristian@gmail.com>
2024-01-08 18:42:58 +0000

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- color of address bar in mobile browser -->
+    <meta name="theme-color" content="#2B2B35">
+    <!-- favicon  -->
+    <link rel="shortcut icon" href="{{ asset('img/thumbnail.png') }}" type="image/x-icon">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="{{ asset('css/plugins/bootstrap.min.css') }}">
+    <!-- font awesome css -->
+    <link rel="stylesheet" href="{{ asset('css/plugins/font-awesome.min.css') }}">
+    <!-- swiper css -->
+    <link rel="stylesheet" href="{{ asset('css/plugins/swiper.min.css') }}">
+    <!-- fancybox css -->
+    <link rel="stylesheet" href="{{ asset('css/plugins/fancybox.min.css') }}">
+    <!-- main css -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Christian Djeukeu | 404 </title>
+</head>
+
+<body>
+    <div class="art-app art-app-onepage">
+        <div class="art-app-wrapper">
+            <div class="art-app-container">
+                <div class="art-content">
+                    <div class="art-curtain"></div>
+                    <div class="art-top-bg  bg_image--error" style="background-image: url(img/not-found.png)">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jquery js -->
+    <script src="{{ asset('js/plugins/jquery.min.js') }}"></script>
+    <!-- anime js -->
+    <script src="{{ asset('js/plugins/anime.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('js/plugins/swiper.min.js') }}"></script>
+    <!-- progressbar js -->
+    <script src="{{ asset('js/plugins/progressbar.min.js') }}"></script>
+    <!-- smooth scrollbar js -->
+    <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <!-- overscroll js -->
+    <script src="{{ asset('js/plugins/overscroll.min.js') }}"></script>
+    <!-- typing js -->
+    <script src="{{ asset('js/plugins/typing.min.js') }}"></script>
+    <!-- isotope js -->
+    <script src="{{ asset('js/plugins/isotope.min.js') }}"></script>
+    <!-- fancybox js -->
+    <script src="{{ asset('js/plugins/fancybox.min.js') }}"></script>
+    <!-- swup js -->
+    <script src="{{ asset('js/plugins/swup.min.js') }}"></script>
+
+    <!-- main js -->
+    <script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+
+</html>
