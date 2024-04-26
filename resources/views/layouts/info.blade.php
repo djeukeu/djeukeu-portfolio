17c36f89956eb034a6cd4b9bc840857af4d5198d
djeukeu <dmxchristian@gmail.com>
2024-04-26 09:41:05 +0100

    title

--- resources/views/layouts/info.blade.php
+++ resources/views/layouts/info.blade.php
@@ -19,7 +19,7 @@
                 </div>
             </div>
             <h5 class="art-name mb-10"><a href="/">{{ env('OWNER') }}</a></h5>
-            <div class="art-sm-text">Software Developer <br>Cloud Developer</div>
+            <div class="art-sm-text">Software Developer <br>DevOps</div>
         </div>
 
         <div id="scrollbar2" class="art-scroll-frame">
