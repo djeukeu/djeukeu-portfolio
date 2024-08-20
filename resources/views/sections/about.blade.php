d538bb2bb283e9eda97d0aecf79d777ada28e1bf
djeukeu <dmxchristian@gmail.com>
2024-08-20 13:38:46 +0100

    update statement

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -4,28 +4,14 @@
 
             <div class="art-a art-card">
                 <p class="art-text art-white">Hi 👋 I'm Christian Djeukeu,</p>
-                <p class="art-text art-white">I am a Cameroonian software developer currently based in the UK. During my
-                    time at university, I was always curious to solve real life problems through programming. In June
-                    2021, I started an internship at Arrah Solution, where I was introduced to mobile and web
-                    application development, which was the beginning of my journey in the tech industry. At Arrah
-                    Solution, I quickly became interested in cloud computing technologies such as AWS. So, in my spare
-                    time, I decided to learn more by attending online courses on AWS and Docker & Kubernetes.
+                <p class="art-text art-white">I am meticulous, organised and a Certified AWS Developer with years of
+                    experience in defining requirements, designing, implementing and deploying applications. Using the
+                    tools and knowledge I have gained over the years as a Developer, I oversee the client's project from
+                    the identification of the client's needs to the completion of the project. As a Developer, I have a
+                    wide range of skills, whether it is mobile development, front-end development, back-end development,
+                    database management, UI/UX design or cloud computing.
                 </p>
-                <p class="art-text art-white">As a developer, I like to learn new technologies, regardless of the area
-                    of expertise, be it mobile development, front-end development, back-end development, database
-                    management, UI/UX design and cloud computing.In addition, I gain most of my experience and skills by
-                    taking online courses, working on side projects, and even being a web developer instructor at
-                    GetSmarter. I also like to try new tricks on my side projects.
-                </p>
-                <p class="art-text art-white">I have also faced many challenges during my career growth. Firstly, there
-                    were environmental challenges such as poor lighting and unstable internet connection. Secondly, I
-                    had no one to mentor or even guide me, but at least I have friends and colleagues with whom we share
-                    ideas and learn from each other. I also had the internet to help me in my search.
-                </p>
-                <p class="art-text art-white">Accelerating to today, I have 2+ years of experience in defining
-                    requirements, designing, implementing and deploying applications using various technologies.My
-                    aim is to fine tune my skills and build amazing and high quality applications.
-                </p>
+
             </div>
         </div>
 
