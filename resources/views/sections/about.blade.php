5215144d6dc0a80a2fd23ca185a48820d2dd9f7c
djeukeu <dmxchristian@gmail.com>
2024-10-19 21:09:50 +0100

    text updated

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -6,12 +6,10 @@
                 <p class="art-text art-white">Hi 👋 I'm Christian Djeukeu,</p>
                 <p class="art-text art-white">I am meticulous, organised and a Certified AWS Developer with years of
                     experience in defining requirements, designing, implementing and deploying applications. Using the
-                    tools and knowledge I have gained over the years as a Developer, I oversee the client's project from
-                    the identification of the client's needs to the completion of the project. As a Developer, I have a
-                    wide range of skills, whether it is mobile development, front-end development, back-end development,
-                    database management, UI/UX design or cloud computing.
-                </p>
-
+                    tools and knowledge I have gained over the years as a developer, I oversee client projects from the
+                    identification of project needs through to completion. As a developer, I have a wide range of skills
+                    in mobile development, front-end development, back-end development, database management, UI/UX
+                    design and cloud computing.</p>
             </div>
         </div>
 
--- resources/views/sections/banner.blade.php
+++ resources/views/sections/banner.blade.php
@@ -7,9 +7,9 @@
                 <div class="art-banner-overlay">
                     <div class="art-banner-title">
                         <h1 class="mb-15">Discover my amazing <br>Project Space</h1>
-                        <div class="art-lg-text art-code mb-25">&lt;<i>code</i>&gt;I
-                            <span class="txt-rotate" data-period="2000"
-                                data-rotate='[ "build amazing websites.", "build iOS and Android applications.", " design beautiful User Interfaces.", "automate my projects." ]'></span>&lt;/<i>code</i>&gt;
+                        <div class="art-lg-text art-code mb-25">&lt;<i>code</i>&gt;<span class="txt-rotate"
+                                data-period="2000"
+                                data-rotate='[ "Build amazing websites.", "Build iOS and Android apps.", "Design beautiful UIs.", "Automate projects." ]'></span>&lt;/<i>code</i>&gt;
                         </div>
                         <div class="art-buttons-frame">
                             <a href="/contact" class="art-btn art-btn-md"><span>Contact Me</span></a>
