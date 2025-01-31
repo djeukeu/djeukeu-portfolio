3ba2b93c7fd06b86b6249696b247efd3e73bf402
djeukeu <dmxchristian@gmail.com>
2025-01-31 09:29:03 +0000

    skills section

--- resources/views/sections/skills.blade.php
+++ resources/views/sections/skills.blade.php
@@ -13,9 +13,12 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Mobile Development</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/mobile-development.svg') }}" alt="mobile-development"
+                        class="skill-icon mb-10">
+                    <h5 class="mb-10">Mobile Development</h5>
+                    <p class="mb-10">Code cross-platform mobile applications for optimal performance and integrate
+                        features such as push notifications, chat messaging and GPS.</p>
                 </div>
             </div>
         </div>
@@ -24,9 +27,12 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Front-end Development</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/front-end-development.svg') }}" alt="mobile-development"
+                        class="skill-icon mb-10">
+                    <h5 class="mb-10">Front-end Development</h5>
+                    <p class="mb-10">Build visual and interactive websites and web applications by optimising design,
+                        responsiveness and performance across devices and browsers.</p>
                 </div>
             </div>
         </div>
@@ -34,9 +40,12 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Back-end Development</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/back-end-development.svg') }}" alt="mobile-development"
+                        class="skill-icon mb-10">
+                    <h5 class="mb-10">Back-end Development</h5>
+                    <p class="mb-10">Build and maintain the server-side logic, databases and APIs that power web
+                        applications, ensuring data processing, security and smooth communication.</p>
                 </div>
             </div>
         </div>
@@ -45,9 +54,11 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>UI / UX Design</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/ui-ux-design.svg') }}" alt="mobile-development" class="skill-icon mb-10">
+                    <h5 class="mb-10">UI / UX Design</h5>
+                    <p class="mb-10">Create visually appealing, user-friendly interfaces, aesthetic layouts and ensure
+                        intuitive navigation, usability and accessibility of digital products.</p>
                 </div>
             </div>
         </div>
@@ -56,9 +67,11 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Cloud Computing</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/cloud-computing.svg') }}" alt="mobile-development" class="skill-icon mb-10">
+                    <h5 class="mb-10">Cloud Computing</h5>
+                    <p class="mb-10">Deliver scalability, flexibility and cost efficiency with on-demand computing
+                        resources through services such as SaaS, PaaS and IaaS.</p>
                 </div>
             </div>
         </div>
@@ -67,47 +80,16 @@
         <!-- Single Skills -->
         <div class="col-lg-4 col-md-6">
             <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Amazon Web Services (AWS)</h5>
-                    <i class="verify fas fa-check-circle"></i>
+                <div class="art-service-ib-content">
+                    <img src="{{ asset('svg/software-automation.svg') }}" alt="mobile-development"
+                        class="skill-icon mb-10">
+                    <h5 class="mb-10">Software Automation</h5>
+                    <p class="mb-10">Write scripts to perform repetitive tasks to minimise human intervention. This
+                        improves efficiency, accuracy and reduces errors.<p>
                 </div>
             </div>
         </div>
         <!-- Single Skills -->
-
-        <!-- Single Skills -->
-        <div class="col-lg-4 col-md-6">
-            <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Software Automation</h5>
-                    <i class="verify fas fa-check-circle"></i>
-                </div>
-            </div>
-        </div>
-        <!-- Single Skills -->
-
-        <!-- Single Skills -->
-        <div class="col-lg-4 col-md-6">
-            <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Infrastructure Automation</h5>
-                    <i class="verify fas fa-check-circle"></i>
-                </div>
-            </div>
-        </div>
-        <!-- Single Skills -->
-
-        <!-- Single Skills -->
-        <div class="col-lg-4 col-md-6">
-            <div class="art-a art-service-icon-box">
-                <div class="art-service-ib-content verify-skill">
-                    <h5>Infrastructure as Code (IaC)</h5>
-                    <i class="verify fas fa-check-circle"></i>
-                </div>
-            </div>
-        </div>
-        <!-- Single Skills -->
-
 
     </div>
 
